1584fb82ff357b334d7925331641c3b86a497289
NiPeGun <nipegun@gmail.com>
2021-05-16 22:35:56 +0200

    Update UScripts-CrearAlias.sh

--- UScripts-CrearAlias.sh
+++ UScripts-CrearAlias.sh
@@ -5,51 +5,20 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#----------------------------------------------------
-#  Script de NiPeGun para sincronizar los u-scripts
-#----------------------------------------------------
+#-----------------------------------------------------------
+#  Script de NiPeGun para crear los alias de los u-scripts 
+#-----------------------------------------------------------
 
-ColorRojo='\033[1;31m'
-ColorVerde='\033[1;32m'
-FinColor='\033[0m'
+ColorVerde="\033[1;32m"
+FinColor="\033[0m"
 
-# Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
-if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
-    echo ""
-    echo "wget no está instalado. Iniciando su instalación..."
-    echo ""
-    apt-get -y update
-    apt-get -y install wget
-fi
+echo ""
+echo -e "${ColorVerde}Creando alias para los u-scripts...${FinColor}"
+echo ""
 
-# Comprobar si hay conexión a Internet antes de sincronizar los d-scripts
-wget -q --tries=10 --timeout=20 --spider https://github.com
-  if [[ $? -eq 0 ]]; then
-    echo ""
-    echo "---------------------------------------------------------"
-    echo -e "  ${ColorVerde}Sincronizando los u-scripts con las últimas versiones${FinColor}"
-    echo -e "  ${ColorVerde} y descargando nuevos u-scripts si es que existen...${FinColor}"
-    echo "---------------------------------------------------------"
-    echo ""
-    rm /root/scripts/u-scripts -R 2> /dev/null
-    mkdir /root/scripts 2> /dev/null
-    cd /root/scripts
-    git clone --depth=1 https://github.com/nipegun/u-scripts
-    mkdir -p /root/scripts/u-scripts/Alias/
-    rm /root/scripts/u-scripts/.git -R 2> /dev/null
-    find /root/scripts/u-scripts/ -type f -iname "*.sh" -exec chmod +x {} \;
-    /root/scripts/u-scripts/UScripts-CrearAlias.sh
-    find /root/scripts/u-scripts/Alias -type f -exec chmod +x {} \;
-    
-    echo ""
-    echo "-----------------------------------------"
-    echo -e "  ${ColorVerde}u-scripts sincronizados correctamente${FinColor}"
-    echo "-----------------------------------------"
-    echo ""
-  else
-    echo ""
-    echo "---------------------------------------------------------------------------------------------------"
-    echo -e "${ColorRojo}No se pudo iniciar la sincronización de los u-scripts porque no se detectó conexión a Internet.${FinColor}"
-    echo "---------------------------------------------------------------------------------------------------"
-    echo ""
-  fi
+ln -s /root/scripts/u-scripts/SistemaOperativo-Actualizar.sh                          /root/scripts/u-scripts/Alias/aso
+
+echo ""
+echo -e "${ColorVerde}Alias creados. Deberías poder ejecutar los u-scripts escribiendo el nombre de su alias.${FinColor}"
+echo ""
+
