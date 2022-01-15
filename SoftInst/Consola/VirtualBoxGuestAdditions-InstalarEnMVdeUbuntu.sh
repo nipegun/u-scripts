a5c2dfdca8120d8094d01f91d32719f4d4a55b6a
NiPeGun <nipegun@gmail.com>
2022-01-15 12:20:18 +0100

    Update VirtualBoxGuestAdditions-InstalarEnMVdeUbuntu.sh

--- SoftInst/Consola/VirtualBoxGuestAdditions-InstalarEnMVdeUbuntu.sh
+++ SoftInst/Consola/VirtualBoxGuestAdditions-InstalarEnMVdeUbuntu.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#---------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para instalar y configurar las Guest Additions en una máquina vritual Virtual Box de Ubuntu
 #
 #  Ejecución remota:
-#  curl -s x | bash
-#---------------------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/u-scripts/main/SoftInst/Consola/VirtualBoxGuestAdditions-InstalarEnMVdeUbuntu.sh | bash
+#---------------------------------------------------------------------------------------------------------------------------------------------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
