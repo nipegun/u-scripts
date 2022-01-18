27084dbac39464dec813ab4b01ed3152afaaf903
NiPeGun <nipegun@gmail.com>
2022-01-18 09:34:27 +0100

    Update Usuarios-root-DesactivarCuenta.sh

--- Usuarios-root-DesactivarCuenta.sh
+++ Usuarios-root-DesactivarCuenta.sh
@@ -5,18 +5,18 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------
 #  Script de NiPeGun para activar la cuenta del root en Ubuntu
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/u-scripts/main/Usuarios-root-ActivarCuenta.sh | bash
-#--------------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/u-scripts/main/Usuarios-root-DesactivarCuenta.sh | bash
+#-----------------------------------------------------------------------------------------------------------
 
 ## Agregarle una constraseña a la cuenta del root
    echo ""
    echo "  Desactivando la cuenta del root..."
    echo ""
-   sudo passwd root
+   sudo usermod --lock --expiredate 1970-01-02 root
 
 ## Notificar finalización de script
    echo ""
