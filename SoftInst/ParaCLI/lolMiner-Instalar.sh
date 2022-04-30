feebf976b06c1a75869673ce5decc24969f46ee2
NiPeGun <nipegun@gmail.com>
2022-04-30 02:24:27 +0200

    Update lolMiner-Instalar.sh

--- SoftInst/ParaCLI/lolMiner-Instalar.sh
+++ SoftInst/ParaCLI/lolMiner-Instalar.sh
@@ -29,3 +29,27 @@ mv /Mineros/1.48 /Mineros/lolMiner
 sed -i -e 's/WALLET=.*/WALLET=UQBLiO-jvWqwChjFqOsYlGDyZw6G3vYrKOQrgqnVNnTghhRs/' /Mineros/lolMiner/mine_ton.sh
 
 sudo apt-get install xserver-xorg-video-dummy
+
+echo ''                                         > /etc/X11/xorg.conf
+echo 'Section "Device"'                        >> /etc/X11/xorg.conf
+echo '  Identifier  "Configured Video Device"' >> /etc/X11/xorg.conf
+echo '  Driver      "dummy"'                   >> /etc/X11/xorg.conf
+echo 'EndSection'                              >> /etc/X11/xorg.conf
+echo ''                                        >> /etc/X11/xorg.conf
+echo 'Section "Monitor"'                       >> /etc/X11/xorg.conf
+echo '  Identifier  "Configured Monitor"'      >> /etc/X11/xorg.conf
+echo '  HorizSync 31.5-48.5'                   >> /etc/X11/xorg.conf
+echo '  VertRefresh 50-70'                     >> /etc/X11/xorg.conf
+echo 'EndSection'                              >> /etc/X11/xorg.conf
+echo ''                                        >> /etc/X11/xorg.conf
+echo 'Section "Screen"'                        >> /etc/X11/xorg.conf
+echo '  Identifier  "Default Screen"'          >> /etc/X11/xorg.conf
+echo '  Monitor     "Configured Monitor"'      >> /etc/X11/xorg.conf
+echo '  Device      "Configured Video Device"' >> /etc/X11/xorg.conf
+echo '  DefaultDepth 24'                       >> /etc/X11/xorg.conf
+echo '  SubSection "Display"'                  >> /etc/X11/xorg.conf
+echo '  Depth 24'                              >> /etc/X11/xorg.conf
+echo '  Modes "1024x800"'                      >> /etc/X11/xorg.conf
+echo '  EndSubSection'                         >> /etc/X11/xorg.conf
+echo 'EndSection'                              >> /etc/X11/xorg.conf
+
