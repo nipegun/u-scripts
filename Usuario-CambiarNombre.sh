5d94d8c94b6d913f29920a08462463752d045a25
NiPeGun <nipegun@gmail.com>
2022-10-23 14:08:38 +0200

    Create Usuario-CambiarNombre.sh

---
+++ Usuario-CambiarNombre.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para cambiar el nombre de un usuario
+#
+#  Ejecución con curl:
+#  curl --silent https://raw.githubusercontent.com/nipegun/d-scripts/master/Usuario-CambiarNombre.sh | bash -s UsuarioViejo UsuarioNuevo
+# ----------
+
+CantArgsCorrectos=2
+ArgsInsuficientes=65
+
+if [ $# -ne $CantArgsCorrectos ]
+  then
+    echo ""
+    echo "------------------------------------------------------------------------------"
+    echo "Mal uso del script."
+    echo ""
+    echo "El uso correcto sería: cndu [NombreDeUsuarioViejo] [NombreDeUsuarioNuevo]"
+    echo ""
+    echo "Ejemplo:"
+    echo ' cndu pepe jose'
+    echo "------------------------------------------------------------------------------"
+    echo ""
+    exit $ArgsInsuficientes
+  else
+    echo ""
+    usermod -l $2 $1
+    usermod -d /home/$2 -m $2
+    groupmod -n $2 $1
+    echo ""
+fi
