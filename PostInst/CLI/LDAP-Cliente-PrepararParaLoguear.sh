b41146cd78f2f784c14ff3bf3153ea216bbe1b57
NiPeGun <nipegun@gmail.com>
2022-11-16 00:02:56 +0100

    Update LDAP-Cliente-PrepararParaLoguear.sh

--- PostInst/CLI/LDAP-Cliente-PrepararParaLoguear.sh
+++ PostInst/CLI/LDAP-Cliente-PrepararParaLoguear.sh
@@ -27,7 +27,7 @@ sudo rm -f /etc/ldap/ldap.conf
 sudo mv ~/ldap.conf /etc/ldap/ldap.conf
 sudo chown root:root /etc/ldap/ldap.conf
 
-sudo sed -i -e 's|systemd|ldap|g' /etc/nsswitch.conf
+sudo sed -i -e 's|systemd|ldap|g'                  /etc/nsswitch.conf
 sudo sed -i -e 's|^passwd:.*|passwd: files ldap|g' /etc/nsswitch.conf
 sudo sed -i -e 's|^group:.*|group: files ldap|g'   /etc/nsswitch.conf
 sudo sed -i -e 's|^shadow:.*|shadow: files ldap|g' /etc/nsswitch.conf
