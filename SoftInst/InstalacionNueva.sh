7b5a19c447a71224719e909b31d1cdb78de83a30
NiPeGun <nipegun@gmail.com>
2022-11-25 16:55:41 +0100

    Update InstalacionNueva.sh

--- SoftInst/InstalacionNueva.sh
+++ SoftInst/InstalacionNueva.sh
@@ -12,57 +12,59 @@
 #  curl -s x | bash
 #--------------------------------------------------------------------
 
-ColorRojo='\033[1;31m'
-ColorVerde='\033[1;32m'
-FinColor='\033[0m'
+vColorAzul="\033[0;34m"
+vColorAzulClaro="\033[1;34m"
+vColorVerde='\033[1;32m'
+vColorRojo='\033[1;31m'
+vFinColor='\033[0m'
 
-## Determinar la versión de Ubuntu
-   VersUbuntu=$(cat /etc/lsb-release | grep ODENAME | head -n1 | cut -d'=' -f2)
+# Determinar la versión de Ubuntu
+  vVersUbuntu=$(cat /etc/lsb-release | grep ODENAME | head -n1 | cut -d'=' -f2)
 
-if [ $VersUbuntu == "trusty" ]; then
+if [ $vVersUbuntu == "trusty" ]; then
 
   echo ""
-  echo -e "${ColorVerde}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 14.04 LTS (Trusty Tahr)...${FinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 14.04 LTS (Trusty Tahr)...${vFinColor}"
   echo ""
 
   echo ""
   echo "  Comandos para Ubuntu 14.04 LTS todavía no preparados. Prueba ejecutarlo en otra versión de Ubuntu."
   echo ""
 
-elif [ $VersUbuntu == "xenial" ]; then
+elif [ $vVersUbuntu == "xenial" ]; then
 
   echo ""
-  echo -e "${ColorVerde}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 16.04 LTS (Xenial Xerus)...${FinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 16.04 LTS (Xenial Xerus)...${vFinColor}"
   echo ""
 
   echo ""
   echo "  Comandos para Ubuntu 16.04 LTS todavía no preparados. Prueba ejecutarlo en otra versión de Ubuntu."
   echo ""
 
-elif [ $VersUbuntu == "bionic" ]; then
+elif [ $vVersUbuntu == "bionic" ]; then
 
   echo ""
-  echo -e "${ColorVerde}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 18.04 LTS (Bionic Beaver)...${FinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 18.04 LTS (Bionic Beaver)...${vFinColor}"
   echo ""
 
   echo ""
   echo "  Comandos para Ubuntu 18.04 LTS todavía no preparados. Prueba ejecutarlo en otra versión de Ubuntu."
   echo ""
 
-elif [ $VersUbuntu == "focal" ]; then
+elif [ $vVersUbuntu == "focal" ]; then
 
   echo ""
-  echo -e "${ColorVerde}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 20.04 LTS (Focal Fossa)...${FinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 20.04 LTS (Focal Fossa)...${vFinColor}"
   echo ""
 
   echo ""
   echo "  Comandos para Ubuntu 20.04 LTS todavía no preparados. Prueba ejecutarlo en otra versión de Ubuntu."
   echo ""
 
-elif [ $VersUbuntu == "jammy" ]; then
+elif [ $vVersUbuntu == "jammy" ]; then
 
   echo ""
-  echo -e "${ColorVerde}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 22.04 LTS (Jammy Jellyfish)...${FinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 22.04 LTS (Jammy Jellyfish)...${vFinColor}"
   echo ""
 
   echo ""
@@ -72,7 +74,7 @@ elif [ $VersUbuntu == "jammy" ]; then
 else
 
   echo ""
-  echo -e "${ColorRojo}  Los u-scripts no se pueden ejecutar en versiones de Ubuntu que no sean LTS...${FinColor}"
+  echo -e "${vColorRojo}  Los u-scripts no se pueden ejecutar en versiones de Ubuntu que no sean LTS...${vFinColor}"
   echo ""
 
 fi
