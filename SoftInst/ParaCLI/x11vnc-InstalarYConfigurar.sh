62569116c9b747b6c6612958041856b6dec47dda
NiPeGun <nipegun@gmail.com>
2022-11-25 17:36:08 +0100

    Update x11vnc-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/x11vnc-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/x11vnc-InstalarYConfigurar.sh
@@ -80,18 +80,20 @@ elif [ $vVersUbuntu == "jammy" ]; then
   echo ""
   echo "  Creando el servicio de systemd..."
   echo ""
-  echo "[Unit]"                                                                      > /lib/systemd/system/x11vnc.service
-  echo "Description=x11vnc service"                                                 >> /lib/systemd/system/x11vnc.service
-  echo "After=display-manager.service network.target syslog.target"                 >> /lib/systemd/system/x11vnc.service
-  echo ""                                                                           >> /lib/systemd/system/x11vnc.service
-  echo "[Service]"                                                                  >> /lib/systemd/system/x11vnc.service
-  echo "Type=simple"                                                                >> /lib/systemd/system/x11vnc.service
-  echo "ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd Pass123" >> /lib/systemd/system/x11vnc.service
-  echo "ExecStop=/usr/bin/killall x11vnc"                                           >> /lib/systemd/system/x11vnc.service
-  echo "Restart=on-failure"                                                         >> /lib/systemd/system/x11vnc.service
-  echo ""                                                                           >> /lib/systemd/system/x11vnc.service
-  echo "[Install]"                                                                  >> /lib/systemd/system/x11vnc.service
-  echo "WantedBy=multi-user.target"                                                 >> /lib/systemd/system/x11vnc.service
+  echo "[Unit]"                                                                                                                                            > /lib/systemd/system/x11vnc.service
+  echo "Description=x11vnc service"                                                                                                                       >> /lib/systemd/system/x11vnc.service
+  echo "After=display-manager.service network.target syslog.target"                                                                                       >> /lib/systemd/system/x11vnc.service
+  echo ""                                                                                                                                                 >> /lib/systemd/system/x11vnc.service
+  echo "[Service]"                                                                                                                                        >> /lib/systemd/system/x11vnc.service
+  echo "Type=simple"                                                                                                                                      >> /lib/systemd/system/x11vnc.service
+# echo "ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd Pass123"                                                                       >> /lib/systemd/system/x11vnc.service
+# echo "ExecStart=/usr/bin/x11vnc -display :0 -forever -shared -rfbauth /etc/x11vnc.passwd"                                                               >> /lib/systemd/system/x11vnc.service
+  echo "ExecStart=/usr/bin/x11vnc -display :0 -auth guess -forever -loop -noxdamage -repeat -localhost -rfbauth /etc/x11vnc.passwd -rfbport 5900 -shared" >> /lib/systemd/system/x11vnc.service
+  echo "ExecStop=/usr/bin/killall x11vnc"                                                                                                                 >> /lib/systemd/system/x11vnc.service
+  echo "Restart=on-failure"                                                                                                                               >> /lib/systemd/system/x11vnc.service
+  echo ""                                                                                                                                                 >> /lib/systemd/system/x11vnc.service
+  echo "[Install]"                                                                                                                                        >> /lib/systemd/system/x11vnc.service
+  echo "WantedBy=multi-user.target"                                                                                                                       >> /lib/systemd/system/x11vnc.service
 
   echo ""
   echo "  Recargando la configuración de los daemons..."
@@ -106,7 +108,8 @@ elif [ $vVersUbuntu == "jammy" ]; then
   echo ""
   echo "  Creando contraseña de usuario para x11vnc..."
   echo ""
-  x11vnc -storepasswd
+  x11vnc -storepasswd /etc/x11vnc.passwd
+  chmod 0400 /etc/x11vnc.passwd
 
 
 
