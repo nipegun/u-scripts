8aa4f5c3dbeae225c3d983412f0837dd22619774
NiPeGun <nipegun@gmail.com>
2022-11-25 20:21:33 +0100

    Update x11vnc-InstalarYConfigurar.sh

--- SoftInst/ParaCLI/x11vnc-InstalarYConfigurar.sh
+++ SoftInst/ParaCLI/x11vnc-InstalarYConfigurar.sh
@@ -92,9 +92,11 @@ elif [ $vVersUbuntu == "jammy" ]; then
   echo ""                                                                                                                                                 >> /etc/systemd/system/x11vnc.service
   echo "[Service]"                                                                                                                                        >> /etc/systemd/system/x11vnc.service
   echo "Type=simple"                                                                                                                                      >> /etc/systemd/system/x11vnc.service
-  echo "ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd Pass123"                                                                       >> /etc/systemd/system/x11vnc.service
+# echo "ExecStart=/usr/bin/x11vnc -forever -display :0 -auth guess -passwd Pass123"                                                                       >> /etc/systemd/system/x11vnc.service
 # echo "ExecStart=/usr/bin/x11vnc -display :0 -forever -shared -rfbauth /etc/x11vnc.passwd"                                                               >> /etc/systemd/system/x11vnc.service
 # echo "ExecStart=/usr/bin/x11vnc -display :0 -auth guess -forever -loop -noxdamage -repeat -localhost -rfbauth /etc/x11vnc.passwd -rfbport 5900 -shared" >> /etc/systemd/system/x11vnc.service
+  echo "ExecStart=/usr/bin/x11vnc -auth /run/user/1000/gdm/Xauthority -passwd Pass123"                                                               >> /etc/systemd/system/x11vnc.service
+  echo "ExecStart=/usr/bin/x11vnc -auth /run/user/1000/gdm/Xauthority -rfbauth /etc/x11vnc.passwd"                                                               >> /etc/systemd/system/x11vnc.service
   echo "ExecStop=/usr/bin/killall x11vnc"                                                                                                                 >> /etc/systemd/system/x11vnc.service
   echo "Restart=on-failure"                                                                                                                               >> /etc/systemd/system/x11vnc.service
   echo ""                                                                                                                                                 >> /etc/systemd/system/x11vnc.service
