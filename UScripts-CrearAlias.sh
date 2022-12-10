f28b65ffdad91960d57255d79ce93133706de1c4
NiPeGun <nipegun@gmail.com>
2022-12-10 15:46:59 +0100

    Update UScripts-CrearAlias.sh

--- UScripts-CrearAlias.sh
+++ UScripts-CrearAlias.sh
@@ -5,18 +5,21 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------
+# ----------
 #  Script de NiPeGun para crear los alias de los u-scripts 
-#-----------------------------------------------------------
+# ----------
 
 ColorVerde="\033[1;32m"
 FinColor="\033[0m"
 
 echo ""
-echo -e "${ColorVerde}Creando alias para los u-scripts...${FinColor}"
+echo -e "${ColorVerde}  Creando alias para los u-scripts...${FinColor}"
 echo ""
 
-ln -s ~/scripts/u-scripts/SistemaOperativo-Actualizar.sh             ~/scripts/u-scripts/Alias/aso
+ln -s ~/scripts/u-scripts/SistemaOperativo-Actualizar.sh            ~/scripts/u-scripts/Alias/aso
+ln -s ~/scripts/u-scripts/SistemaOperativo-ActualizaryReiniciar.sh  ~/scripts/u-scripts/Alias/asoyr
+ln -s ~/scripts/u-scripts/SistemaOperativo-Reiniciar.sh             ~/scripts/u-scripts/Alias/rso
+
 ln -s ~/scripts/u-scripts/UScripts-Sincronizar.sh                    ~/scripts/u-scripts/Alias/sinus
 
 echo ""
