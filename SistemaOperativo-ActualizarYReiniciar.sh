cfc5842e019d7a8bfe09315ce3f237fee3df75ed
NiPeGun <nipegun@gmail.com>
2022-12-10 15:39:04 +0100

    Create SistemaOperativo-ActualizarYReiniciar.sh

---
+++ SistemaOperativo-ActualizarYReiniciar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para actualizar e inmediamente reiniciar Ubuntu
+# ----------
+
+~/scripts/d-scripts/SistemaOperativo-Actualizar.sh
+~/scripts/d-scripts/SistemaOperativo-Reiniciar.sh
+
