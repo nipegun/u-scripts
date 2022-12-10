3a1e32d4419d48d935f0187ff6d145da868f1a00
NiPeGun <nipegun@gmail.com>
2022-12-10 15:42:27 +0100

    Update SistemaOperativo-Actualizar.sh

--- SistemaOperativo-Actualizar.sh
+++ SistemaOperativo-Actualizar.sh
@@ -5,19 +5,19 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para actualizar Ubuntu
 #
 #  Ejecución remota:
 #  curl -s https://raw.githubusercontent.com/nipegun/u-scripts/main/SistemaOperativo-Actualizar.sh | bash
-#--------------------------------------------------------------------------------------------------------
+# ----------
 
 ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
 FinColor='\033[0m'
 
 echo ""
-echo -e "${ColorVerde}  Iniciando el script de actualización del sistema operativo...${FinColor}"
+echo -e "${ColorVerde}  Iniciando el script de actualización de Ubuntu...${FinColor}"
 echo ""
 
 echo ""
