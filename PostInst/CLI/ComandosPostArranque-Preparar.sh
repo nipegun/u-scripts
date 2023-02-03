786033379c402f9999ed19ef39b9115f10d54fb6
NiPeGun <nipegun@gmail.com>
2023-02-03 13:17:38 +0100

    Update ComandosPostArranque-Preparar.sh

--- PostInst/CLI/ComandosPostArranque-Preparar.sh
+++ PostInst/CLI/ComandosPostArranque-Preparar.sh
@@ -6,7 +6,7 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-#  Script de NiPeGun para instalar y configurar xxxxxxxxx en Ubuntu
+#  Script de NiPeGun para instalar y configurar los comandos postarranque en Ubuntu
 #
 #  Ejecución remota:
 #  curl -s x | bash
@@ -113,8 +113,55 @@ elif [ $VersUbuntu == "jammy" ]; then
   echo ""
 
   echo ""
-  echo "  Comandos para Ubuntu 22.04 LTS todavía no preparados. Prueba ejecutarlo en otra versión de Ubuntu."
+  echo "  Configurando el servicio..."
   echo ""
+  sudo su -c 'echo "[Unit]"                                   > /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo "Description=/etc/rc.local Compatibility" >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo "ConditionPathExists=/etc/rc.local"       >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo ""                                        >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo "[Service]"                               >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo "Type=forking"                            >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo "ExecStart=/etc/rc.local start"           >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo "TimeoutSec=0"                            >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo "StandardOutput=tty"                      >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo "RemainAfterExit=yes"                     >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo "SysVStartPriority=99"                    >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo ""                                        >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo "[Install]"                               >> /etc/systemd/system/rc-local.service'
+  sudo su -c 'echo "WantedBy=multi-user.target"              >> /etc/systemd/system/rc-local.service'
+
+  echo ""
+  echo "  Creando el archivo /etc/rc.local..."
+  echo ""
+  sudo su -c "echo '#!/bin/bash'                            > /etc/rc.local"
+  sudo su -c 'echo ""                                      >> /etc/rc.local'
+  sudo su -c 'echo "/root/scripts/ComandosPostArranque.sh" >> /etc/rc.local'
+  sudo su -c 'echo "exit 0"                                >> /etc/rc.local'
+  sudo chmod +x /etc/rc.local
+
+  echo ""
+  echo "  Creando el archivo para meter los comandos..."
+  echo ""
+  sudo mkdir -p /root/scripts/ 2> /dev/null
+  sudo su -c "echo '#!/bin/bash'                                                                                         > /root/scripts/ComandosPostArranque.sh"
+  sudo su -c 'echo ""                                                                                                   >> /root/scripts/ComandosPostArranque.sh'
+  sudo su -c 'echo ""                                                                                                   >> /root/scripts/ComandosPostArranque.sh'
+  sudo su -c "echo 'FechaDeEjec="'$(date +A%YM%mD%d@%T)'"'                                                              >> /root/scripts/ComandosPostArranque.sh"
+  sudo su -c 'echo ""                                                                                                   >> /root/scripts/ComandosPostArranque.sh'
+  sudo su -c 'echo 'echo "Iniciada la ejecución del script post-arranque el $FechaDeEjec" >> /var/log/PostArranque.log' >> /root/scripts/ComandosPostArranque.sh'
+  sudo su -c 'echo ""                                                                                                   >> /root/scripts/ComandosPostArranque.sh'
+  sudo su -c 'echo "#  ESCRIBE ABAJO, UNA POR LÍNEA, LAS TAREAS A EJECUTAR DESPUÉS DE CADA ARRANQUE"                    >> /root/scripts/ComandosPostArranque.sh'
+  sudo su -c 'echo "#▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼"                  >> /root/scripts/ComandosPostArranque.sh'
+  sudo su -c 'echo ""                                                                                                   >> /root/scripts/ComandosPostArranque.sh'
+  sudo chmod 700 /root/scripts/ComandosPostArranque.sh
+  sudo touch /var/log/PostArranque.log
+
+  echo ""
+  echo "  Activando y arrancando el servicio..."
+  echo ""
+  sudo systemctl enable rc-local
+  sudo systemctl start rc-local.service
+  sudo systemctl status rc-local.service
 
 else
 
