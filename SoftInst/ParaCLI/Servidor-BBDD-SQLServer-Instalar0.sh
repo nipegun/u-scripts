5a1400dbf4b0e0221c9d888c2b121bdc6d4c2102
NiPeGun <nipegun@gmail.com>
2023-02-12 03:24:36 +0100

    Update and rename Servidor-BBDD-SQLServer-Instalar.sh to Servidor-BBDD-SQLServer-Instalar0.sh

--- SoftInst/ParaCLI/Servidor-BBDD-SQLServer-Instalar0.sh
+++ SoftInst/ParaCLI/Servidor-BBDD-SQLServer-Instalar0.sh
@@ -6,7 +6,7 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-#  Script de NiPeGun para instalar y configurar xxxxxxxxx en Ubuntu
+#  Script de NiPeGun para instalar y configurar SQLServer en Ubuntu
 #
 #  Ejecución remota:
 #  curl -s https://raw.githubusercontent.com/nipegun/u-scripts/main/SoftInst/ParaCLI/Servidor-BBDD-SQLServer-Instalar.sh | bash
@@ -24,7 +24,7 @@ vFinColor='\033[0m'
 if [ $vVersUbuntu == "trusty" ]; then
 
   echo ""
-  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 14.04 LTS (Trusty Tahr)...${vFinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de SQLServer para Ubuntu 14.04 LTS (Trusty Tahr)...${vFinColor}"
   echo ""
 
   echo ""
@@ -34,7 +34,7 @@ if [ $vVersUbuntu == "trusty" ]; then
 elif [ $vVersUbuntu == "xenial" ]; then
 
   echo ""
-  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 16.04 LTS (Xenial Xerus)...${vFinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de SQLServer para Ubuntu 16.04 LTS (Xenial Xerus)...${vFinColor}"
   echo ""
 
   echo ""
@@ -44,7 +44,7 @@ elif [ $vVersUbuntu == "xenial" ]; then
 elif [ $vVersUbuntu == "bionic" ]; then
 
   echo ""
-  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 18.04 LTS (Bionic Beaver)...${vFinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de SQLServer para Ubuntu 18.04 LTS (Bionic Beaver)...${vFinColor}"
   echo ""
 
   echo ""
@@ -54,17 +54,87 @@ elif [ $vVersUbuntu == "bionic" ]; then
 elif [ $vVersUbuntu == "focal" ]; then
 
   echo ""
-  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 20.04 LTS (Focal Fossa)...${vFinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de SQLServer para Ubuntu 20.04 LTS (Focal Fossa)...${vFinColor}"
   echo ""
 
-  echo ""
-  echo "  Comandos para Ubuntu 20.04 LTS todavía no preparados. Prueba ejecutarlo en otra versión de Ubuntu."
-  echo ""
+  # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+    if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+      echo ""
+      echo -e "${vColorRojo}    El paquete curl no está instalado. Iniciando su instalación...${vFinColor}"
+      echo ""
+      apt-get -y update
+      apt-get -y install curl
+      echo ""
+    fi
+  # Agregar el repositorio
+    curl -sL https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2022.list > /etc/apt/sources.list.d/mssql-server-2022.list
+  # Agregar la llave
+    # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo -e "${vColorRojo}    El paquete wget no está instalado. Iniciando su instalación...${vFinColor}"
+        echo ""
+        apt-get -y update
+        apt-get -y install wget
+        echo ""
+      fi
+    # Comprobar si el paquete gnupg2 está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s gnupg2 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo -e "${vColorRojo}    El paquete gnupg2 no está instalado. Iniciando su instalación...${vFinColor}"
+        echo ""
+        apt-get -y update
+        apt-get -y install gnupg2
+        echo ""
+      fi
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+  # Instalar el paquete
+    apt-get -y update
+    apt-get -y install mssql-server
+  # Configurar
+    /opt/mssql/bin/mssql-conf setup
+  # Estado del servicio
+    systemctl status mssql-server --no-pager
+  # Instalar herramientas
+    # Agregar repositorio
+      curl -sL https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/msprod.list
+    # Agregar llave
+      curl -sL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+    # Actualizar
+      apt-get -y update
+    # Instalar paquetes
+      apt-get -y install mssql-tools unixodbc-dev
+    # Agregar la variable de entorno
+      sh -c "echo 'export PATH=$PATH:/opt/mssql-tools/bin/' >> /root/.bash_profile"
+      sh -c "echo 'export PATH=$PATH:/opt/mssql-tools/bin/' >> /root/.bashrc"
+      source /root/.bashrc
+  # Quitar permiso de ejecución al archivo del servicio
+    chmod -x /usr/lib/systemd/system/mssql-server.service
+  # Notificar de fin de la instalación
+    echo ""
+    echo "  Instalación finalizada..."
+    echo ""
+    echo "  Para conectarte a la CLI de SQLServer ejecuta:"
+    echo ""
+    echo "    sqlcmd -S localhost -U sa -P 'Contraseña'"
+    echo "    o"
+    echo "    sqlcmd -S localhost -U sa"
+    echo ""
+    echo "  ...para hacer que pida la contraseña."
+    echo ""
+    echo "  NOTA: sa (System Administrator) es el root."
+    echo ""
+    echo "  Para ver el estado del servicio, ejecuta: "
+    echo "    systemctl status  mssql-server.service --no-pager"
+    #sp_configure @configname = 'remote admin connections', @configvalue = 1;
+    #go
+    #reconfigure;
+    #go
 
 elif [ $vVersUbuntu == "jammy" ]; then
 
   echo ""
-  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de xxxxxxxxx para Ubuntu 22.04 LTS (Jammy Jellyfish)...${vFinColor}"
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de SQLServer para Ubuntu 22.04 LTS (Jammy Jellyfish)...${vFinColor}"
   echo ""
 
   # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
