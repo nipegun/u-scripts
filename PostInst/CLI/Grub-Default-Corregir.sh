0fc87f6ba33ddc83439084c1653daf09aeca0b08
NiPeGun <nipegun@gmail.com>
2023-03-01 18:03:12 +0100

    Update Grub-Default-Corregir.sh

--- PostInst/CLI/Grub-Default-Corregir.sh
+++ PostInst/CLI/Grub-Default-Corregir.sh
@@ -3,4 +3,5 @@
 sudo cp /etc/default/grub /etc/default/grub.bak
 sudo sed -i -e 's|^GRUB_TIMEOUT_STYLE=.*|GRUB_DISABLE_OS_PROBER=false"|g' /etc/default/grub
 sudo sed -i -e 's|^GRUB_TIMEOUT=.*|GRUB_TIMEOUT=2|g'                      /etc/default/grub
+sudo update-grub
 
