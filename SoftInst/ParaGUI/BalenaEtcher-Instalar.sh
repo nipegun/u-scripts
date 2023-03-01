7e5a6d9b15ad9121d557d3dbdb6540cdc13108d3
NiPeGun <nipegun@gmail.com>
2023-03-01 15:35:34 +0100

    Create BalenaEtcher-Instalar.sh

---
+++ SoftInst/ParaGUI/BalenaEtcher-Instalar.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para instalar y configurar BalenaEtcher en Ubuntu
+#
+#  Ejecución remota:
+#  curl -s x | bash
+# ----------
+
+vColorAzul="\033[0;34m"
+vColorAzulClaro="\033[1;34m"
+vColorVerde='\033[1;32m'
+vColorRojo='\033[1;31m'
+vFinColor='\033[0m'
+
+# Determinar la versión de Ubuntu
+  vVersUbuntu=$(cat /etc/lsb-release | grep ODENAME | head -n1 | cut -d'=' -f2)
+
+if [ $vVersUbuntu == "trusty" ]; then
+
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de BalenaEtcher para Ubuntu 14.04 LTS (Trusty Tahr)...${vFinColor}"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Ubuntu 14.04 LTS todavía no preparados. Prueba ejecutarlo en otra versión de Ubuntu."
+  echo ""
+
+elif [ $vVersUbuntu == "xenial" ]; then
+
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de BalenaEtcher para Ubuntu 16.04 LTS (Xenial Xerus)...${vFinColor}"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Ubuntu 16.04 LTS todavía no preparados. Prueba ejecutarlo en otra versión de Ubuntu."
+  echo ""
+
+elif [ $vVersUbuntu == "bionic" ]; then
+
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de BalenaEtcher para Ubuntu 18.04 LTS (Bionic Beaver)...${vFinColor}"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Ubuntu 18.04 LTS todavía no preparados. Prueba ejecutarlo en otra versión de Ubuntu."
+  echo ""
+
+elif [ $vVersUbuntu == "focal" ]; then
+
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de BalenaEtcher para Ubuntu 20.04 LTS (Focal Fossa)...${vFinColor}"
+  echo ""
+
+  echo ""
+  echo "  Comandos para Ubuntu 20.04 LTS todavía no preparados. Prueba ejecutarlo en otra versión de Ubuntu."
+  echo ""
+
+elif [ $vVersUbuntu == "jammy" ]; then
+
+  echo ""
+  echo -e "${vColorAzulClaro}  Iniciando el script de instalación de BalenaEtcher para Ubuntu 22.04 LTS (Jammy Jellyfish)...${vFinColor}"
+  echo ""
+
+  # Instalar descargando el paquete AppImage
+    # Obtener link de descarga
+      # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+        if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+          echo ""
+          echo -e "${vColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${vFinColor}"
+          echo ""
+          sudo apt-get -y update
+          sudo apt-get -y install curl
+          echo ""
+        fi
+      vLink=$(curl -sL https://www.balena.io/etcher#download-etcher | sed 's->->\n-g' | grep href | grep AppImage | grep github | cut -d'`' -f2 | sed 's-ia32-x64-g')
+    # Descargar paquete
+      mkdir -p /root/SoftInst/BalenaEtcher/ 2> /dev/null
+      # Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
+        if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
+          echo ""
+          echo -e "${vColorRojo}  El paquete wget no está instalado. Iniciando su instalación...${vFinColor}"
+          echo ""
+          sudo apt-get -y update
+          sudo apt-get -y install wget
+          echo ""
+        fi
+      wget $vLink -O /root/SoftInst/BalenaEtcher/BlenaEtcher.AppImage
+  # Instalar desde repositorio
+    # Agregar el repositorio
+     # echo "deb https://dl.bintray.com/resin-io/debian stable etcher" >> /etc/apt/sources.list.d/BalenaEtcher.list
+    # Agregar la llave
+      #apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 379CE192D40
+
+else
+
+  echo ""
+  echo -e "${vColorRojo}  Los u-scripts no se pueden ejecutar en versiones de Ubuntu que no sean LTS...${vFinColor}"
+  echo ""
+
+fi
+
