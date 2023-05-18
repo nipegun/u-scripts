842d07e0a0a2c5f67e80b0809aa1d201f4b8f6ee
NiPeGun <nipegun@gmail.com>
2023-05-18 09:40:53 +0200

    Update Proceso-Matar.sh

--- Proceso-Matar.sh
+++ Proceso-Matar.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 if [[ "$1" =~ ^[0-9]+$ ]]; then # Si es número, matar el proceso por número
+  echo ""
+  echo "  Intentando matar el proceso por su n..."
+  echo ""
   pkill $1
 else                            # Si no es número, matarlo por nombre
+  echo ""
+  echo "  Intentando matar el proceso por su nombre..."
+  echo ""
   killall -9 $1
+  echo ""
 fi
