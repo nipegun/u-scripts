c167d1833e298986571bbe4accd48d7c6d14e49e
NiPeGun <nipegun@gmail.com>
2023-05-18 09:09:50 +0200

    Create Software-Instalar.sh

---
+++ PostInst/GUI/Software-Instalar.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+sudo apt-get -y remove xterm
+sudo apt-get -y remove reportbug
+sudo apt-get -y remove blender
+sudo apt-get -y remove imagemagick
+sudo apt-get -y remove inkscape
+sudo apt-get -y autoremove
+
+sudo apt-get -y install
+sudo apt-get -y install gparted
+sudo apt-get -y install hardinfo
+sudo apt-get -y install bleachbit
+
+sudo apt-get -y install vlc
+sudo apt-get -y install audacity
+sudo apt-get -y install subtitleeditor
+sudo apt-get -y install easytag
+
+sudo apt-get -y install wireshark
+sudo apt-get -y install etherape
+  sudo setcap CAP_NET_RAW=pe /usr/bin/etherape
+sudo apt-get -y install sshpass
+sudo apt-get -y install virt-viewer
+sudo apt-get -y install whois
+sudo apt-get -y install remmina
+sudo apt-get -y install thunderbird
+
+sudo apt-get -y install eiskaltdcpp
+sudo apt-get -y install amule
+sudo apt-get -y install filezilla
+sudo apt-get -y install mumble
+sudo apt-get -y install obs-studio
+
+sudo apt-get -y install scid
+#sudo apt-get -y install scid-rating-data
+#sudo apt-get -y install scid-spell-data
+sudo apt-get -y install stockfishapt-get -y install gnome-tweaks
+sudo apt-get -y install dosbox
+sudo apt-get -y install scummvm
+
+sudo apt-get -y install ghex
+sudo apt-get -y install clamav
+sudo apt-get -y install clamtk
+
+sudo apt-get -y install unrar
+sudo apt-get -y install htop
+sudo apt-get -y install simple-scan
+
+apt-get -y install gnome-tweaks
