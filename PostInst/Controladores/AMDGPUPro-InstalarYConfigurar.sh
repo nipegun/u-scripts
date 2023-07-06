ea9646d0aae603fbb5426d5bcddf6201897c3606
NiPeGun <nipegun@gmail.com>
2023-07-06 13:30:37 +0200

    Update AMDGPUPro-InstalarYConfigurar.sh

--- PostInst/Controladores/AMDGPUPro-InstalarYConfigurar.sh
+++ PostInst/Controladores/AMDGPUPro-InstalarYConfigurar.sh
@@ -3,3 +3,7 @@ sudo chmod 777 amdgpu-install_xxx_all.deb
 sudo apt install amdgpu-install_xxx_all.deb
 sudo apt-get update
 sudo amdgpu-install -y --accept-eula --usecase=opencl --opencl=rocr,legacy
+#sudo usermod -a -G render $LOGNAME
+sudo usermod -a -G render nipegun
+#sudo usermod -a -G video $LOGNAME
+sudo usermod -a -G video nipegun
