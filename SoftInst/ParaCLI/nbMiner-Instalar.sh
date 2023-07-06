15025c4d56141c1c12229c89b7f13cf41b2cc96a
NiPeGun <nipegun@gmail.com>
2023-07-06 13:11:47 +0200

    Update nbMiner-Instalar.sh

--- SoftInst/ParaCLI/nbMiner-Instalar.sh
+++ SoftInst/ParaCLI/nbMiner-Instalar.sh
@@ -6,33 +6,30 @@
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
 # ----------
-#  Script de NiPeGun para instalar y configurar nbMiner en Ubuntu
+# Script de NiPeGun para instalar y configurar NBMiner en Ubuntu para minar RVN
 #
-#  Para minar RVN
-#
-#  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/u-scripts/main/SoftInst/ParaCLI/nbMiner-Instalar.sh | sudo bash
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/u-scripts/main/SoftInst/ParaCLI/nbMiner-Instalar.sh | sudo bash
 # ----------
 
-ColorRojo='\033[1;31m'
-ColorVerde='\033[1;32m'
-FinColor='\033[0m'
+# Descargar archivos
+  #vUltTag=$(curl -sL https://github.com/NebuTech/NBMiner/releases/latest  | cut -d'"' -f2)
+  #vArchivo=$(curl -sL $vUltTag | grep href | grep inux | grep -v "sha" | cut -d'"' -f2)
+  vURLArchivoUltVers=$(curl -sL https://nbminer.com | grep "dl.nbminer" | grep inux | head -n1 | cut -d'"' -f2)
+  sudo apt-get -y install wget
+  sudo mkdir -p /Mineros/ 2 >/dev/null
+  sudo wget $vURLArchivoUltVers -O /Mineros/nbMiner.tar.gz
 
-#vUltTag=$(curl -sL https://github.com/NebuTech/NBMiner/releases/latest  | cut -d'"' -f2)
-#vArchivo=$(curl -sL $vUltTag | grep href | grep inux | grep -v "sha" | cut -d'"' -f2)
-vURLArchivoUltVers=$(curl -sL https://nbminer.com | grep "dl.nbminer" | grep inux | head -n1 | cut -d'"' -f2)
-sudo apt-get -y install wget
-sudo mkdir -p /Mineros/ 2 >/dev/null
-sudo wget $vURLArchivoUltVers -O /Mineros/nbMiner.tar.gz
-sudo apt-get -y install tar
-sudo tar -xvf /Mineros/nbMiner.tar.gz -C /Mineros/
-sudo mv /Mineros/NBMiner_Linux /Mineros/NBMiner
+# Descomprimir archivos
+  sudo apt-get -y install tar
+  sudo tar -xvf /Mineros/nbMiner.tar.gz -C /Mineros/
+  sudo mv /Mineros/NBMiner_Linux /Mineros/NBMiner
 
-echo '#!/bin/bash' > /Mineros/nbMiner/mine_rvn-woolypooly.sh
-echo "/Mineros/nbMiner/nbminer -a kawpow -o stratum+tcp://pool.eu.woolypooly.com:55555 -u RKxPhh36Cz6JoqMuq1nwMuPYnkj8DmUswy.YestonRX560-4GB -p x" >> /Mineros/nbMiner/mine_rvn-woolypooly.sh
-chmod 777 /Mineros/nbMiner/mine_rvn-woolypooly.sh
+# Crear los scripts para minar
+  sudo echo '#!/bin/bash'                                                                                                                                  > /Mineros/NBMiner/mine_rvn-woolypooly.sh
+  sudo echo "/Mineros/NBMiner/nbminer -a kawpow -o stratum+tcp://pool.eu.woolypooly.com:55555 -u RKxPhh36Cz6JoqMuq1nwMuPYnkj8DmUswy.YestonRX560-4GB -p x" >> /Mineros/NBMiner/mine_rvn-woolypooly.sh
+  sudo echo '#!/bin/bash'                                                                                                                                 > /Mineros/NBMiner/mine_rvn-ravenminer.sh
+  sudo echo "/Mineros/NBMiner/nbminer -a kawpow -o stratum+tcp://stratum.ravenminer.com:3838 -u RKxPhh36Cz6JoqMuq1nwMuPYnkj8DmUswy.YestonRX560-4GB -p x" >> /Mineros/NBMiner/mine_rvn-ravenminer.sh
 
-echo '#!/bin/bash' > /Mineros/nbMiner/mine_rvn-ravenminer.sh
-echo "/Mineros/nbMiner/nbminer -a kawpow -o stratum+tcp://eu.ravenminer.com:3838 -u RKxPhh36Cz6JoqMuq1nwMuPYnkj8DmUswy.YestonRX560-4GB -p x" >> /Mineros/nbMiner/mine_rvn-ravenminer.sh
-chmod 777 /Mineros/nbMiner/mine_rvn-ravenminer.sh
-
+# Asignar permisos
+  sudo chmod 777 /Mineros/NBMiner/ -Rv
