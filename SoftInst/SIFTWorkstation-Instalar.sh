8da7a77f9277288c4ef82c8d0560c927bd8edb2d
NiPeGun <nipegun@gmail.com>
2024-10-12 15:19:02 +0200

    Update SIFTWorkstation-Instalar.sh

--- SoftInst/SIFTWorkstation-Instalar.sh
+++ SoftInst/SIFTWorkstation-Instalar.sh
@@ -9,13 +9,7 @@
 #  Script de NiPeGun para instalar y configurar SIFT Workstation en Ubuntu
 #
 # Ejecución remota:
-#   curl -sL x | bash
-#
-# Ejecución remota sin caché:
-#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
-#
-# Ejecución remota con parámetros:
-#   curl -sL x | bash -s Parámetro1 Parámetro2
+#   curl -sL https://raw.githubusercontent.com/nipegun/u-scripts/main/SoftInst/SIFTWorkstation-Instalar.sh | bash
 #
 # Bajar y editar directamente el archivo en nano
 #   curl -sL x | nano -
