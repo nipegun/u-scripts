e3c45a3058b63376d9b07aa3603f407a0bc9fcce
NiPeGun <nipegun@gmail.com>
2025-10-27 17:47:01 +0100

    Update SecureBoot-VerEstado.sh

--- Sistema/SecureBoot-VerEstado.sh
+++ Sistema/SecureBoot-VerEstado.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#--------------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para ver si SecureBoot está activado o no en Ubuntu
 #
 #  Ejecución remota:
-#  curl -s https://raw.githubusercontent.com/nipegun/u-scripts/main/SecureBoot-VerEstado.sh | bash
-#--------------------------------------------------------------------------------------------------------
+#  curl -s https://raw.githubusercontent.com/nipegun/u-scripts/refs/heads/main/Sistema/SecureBoot-VerEstado.sh | bash
+# ----------
 
 mokutil --sb-state
 
