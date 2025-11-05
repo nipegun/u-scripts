48e905bcc394bb1c9f3f6ad250bca7bc81b6404f
NiPeGun <nipegun@gmail.com>
2025-11-05 10:25:54 +0000

    Update TareasCron-Preparar.sh

--- PostInst/CLI/TareasCron-Preparar.sh
+++ PostInst/CLI/TareasCron-Preparar.sh
@@ -22,12 +22,6 @@
   cColorRojo='\033[1;31m'
   cFinColor='\033[0m'
 
-# Comprobar si el script está corriendo como root
-  if [ $(id -u) -ne 0 ]; then
-    echo -e "${cColorRojo}  Este script está preparado para ejecutarse como root y no lo has ejecutado como root...${cFinColor}"
-    exit
-  fi
-
 # Notificar inicio de ejecución del script
   echo ''
   echo -e "${cColorAzulClaro}  Iniciando script para preparar las tareas cron...${cFinColor}"
